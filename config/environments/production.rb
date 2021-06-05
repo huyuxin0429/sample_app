2091284e36ea60f7da27cc3cbfe246416c4542b3
huyuxin0429 <e0564960@u.nus.edu>
2021-06-05 15:10:30 +0800

    switched mailter to mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -122,13 +122,23 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = 'hurailstest.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
+  # ActionMailer::Base.smtp_settings = {
+  # :address => 'smtp.sendgrid.net',
+  # :port => '587',
+  # :authentication => :plain,
+  # :user_name => ENV['SENDGRID_USERNAME'],
+  # :password => ENV['SENDGRID_PASSWORD'],
+  # :domain => 'heroku.com',
+  # :enable_starttls_auto => true
+  # }
+
   ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.sendgrid.net',
-  :port => '587',
-  :authentication => :plain,
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'heroku.com',
-  :enable_starttls_auto => true
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'hurailstest.heroku.com',
+    :authentication => :plain,
   }
+  ActionMailer::Base.delivery_method = :smtp
 end
