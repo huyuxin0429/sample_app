cf6787f5bc699fd1220e5bc128eabdd8e3d9cdc4
huyuxin0429 <e0564960@u.nus.edu>
2021-06-06 22:00:40 +0800

    Added password reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_30_140747) do
+ActiveRecord::Schema.define(version: 2021_06_06_062316) do
+
+  create_table "merchants", force: :cascade do |t|
+    t.string "company_name"
+    t.string "email"
+    t.text "address"
+    t.integer "contact_no"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.string "merchant_remember_digest"
+    t.index ["email"], name: "index_merchants_on_email", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -25,6 +37,8 @@ ActiveRecord::Schema.define(version: 2021_05_30_140747) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
