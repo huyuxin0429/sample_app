70d108ddc30a8ca655e1537de12b4fa212e78d2a
huyuxin0429 <e0564960@u.nus.edu>
2021-06-12 02:32:38 +0800

    updated csrf to deactivate

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::UsersController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    
     def index
         @users = User.all
         render json: @users, only: [:name, :address, :email, :contact_no]
