3fb642b3d462295d77d55107f8a4a1bf4e599ac1
huyuxin0429 <e0564960@u.nus.edu>
2021-06-12 16:27:52 +0800

    updated api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,36 +1,48 @@
 class Api::V1::UsersController < ApplicationController
     skip_before_action :verify_authenticity_token
     
+    # GET /users
     def index
         @users = User.all
         render json: @users, only: [:name, :address, :email, :contact_no]
     end
 
+    # GET /users/:id
     def show
         @user = User.find(params[:id])
         render json: @user, only: [:name, :address, :email, :contact_no]
     end
 
+    # POST /users
     def create
         @user = User.new(user_params)
         if @user.save
             render json: @user, only: [:name, :address, :email, :contact_no]
         else
-            render error: { error: @user.errors.full_messages.join("/n") }, status: 400
+            render json: { status: "error", message: @user.errors.full_messages.join("/n")}, status: 400 
         end
 
     end
 
+    # user = User.find_by(email: params[:session][:email].downcase)
+    # if user && user.authenticate(params[:session][:password])
+
+    # PUT /users
     def update
         @user = User.find(params[:id])
-        if @user
-            @user.update(user_params)
-            render json: { message: "User successfully updated" }, status: 200
+        if @user && @user.authenticate(params[:password])
+            if @user.authenticated?()
+            if @user.update(user_params)
+                render json: { message: "User successfully updated" }, status: 200
+            else
+                render json: { status: "error", message: @user.errors.full_messages.join("/n")}, status: 400 
+            end
         else
-            render error: { error: @user.errors.full_messages.join("/n") }, status: 400
+            render json: { error: "User not found" }, status: 400
         end
     end
 
+    # DELETE /users/:id
     def destroy
         @user = User.find(params[:id])
         if @user
