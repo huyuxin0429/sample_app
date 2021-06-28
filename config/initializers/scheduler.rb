222a6f0b6ddccecea36a873279f2291b06d8b328
huyuxin0429 <e0564960@u.nus.edu>
2021-06-28 16:10:23 +0800

    fixed simulate bug

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -10,7 +10,7 @@ return if defined?(Rails::Console) || Rails.env.test? || File.split($0).last == 
 # Stupid recurrent task...
 #
 s.every DroneHandler.timeDelta.to_s + 's' do
-    DroneHandler.simulate(DroneHandler.timeDelta)
+    DroneHandler.simulate
     # ActionCable.server.broadcast 'map_channel', Drone.all.to_json
 #   Rails.logger.info "hello, it's #{Time.now}"
 #   Rails.logger.flush
