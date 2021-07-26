e285d02daa49f3b1e061587c37f25933bd50cad4
huyuxin0429 <e0564960@u.nus.edu>
2021-07-26 17:08:14 +0800

    added more stations

--- spec/channels/sim_stats_channel_spec.rb
+++ spec/channels/sim_stats_channel_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe SimStatsChannel, type: :channel do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe SimStatsChannel, type: :channel do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
--- spec/models/edge_spec.rb
+++ spec/models/edge_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Edge, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe Edge, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
